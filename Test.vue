45e0b04b1691f7b8b141eef1dc8da2d3afa167ce
hyejin Han <86100654+red2132@users.noreply.github.com>
2025-07-23 13:22:01 +0900

    Create Test.vue

---
+++ Test.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="floating-button" @click="handleClick">
+    +
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FloatingButton',
+  methods: {
+    handleClick() {
+      this.$emit('click') // 외부로 클릭 이벤트 전달
+    }
+  }
+}
+</script>
+
+<style scoped>
+.floating-button {
+  position: fixed;
+  bottom: 24px;
+  right: 24px;
+  width: 56px;
+  height: 56px;
+  background-color: #1976d2;
+  color: white;
+  border-radius: 50%;
+  font-size: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.floating-button:hover {
+  background-color: #115293;
+}
+</style>
